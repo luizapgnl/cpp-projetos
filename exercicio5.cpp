304f8bfa6ba021171f5435f869470d6f69d3a315
luiza :) <138608726+luizapgnl@users.noreply.github.com>
2026-01-04 23:43:33 -0300

    Add menu display function in exercicio5.cpp

---
+++ exercicio5.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Função que exibe o menu
+void exibirMenu() {
+    printf("=====Menu de Opcoes=====\n");
+    printf("1. Cadastrar\n");
+    printf("2. Listar\n");
+    printf("3. Sair\n");
+}
+
+int main() {
+    // Chamada da função que exibe o menu
+    exibirMenu();
+
+    return 0;
+}
